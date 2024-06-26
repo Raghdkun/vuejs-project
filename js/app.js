dc877eb5537be05df1d38fd875eed005f5c31e87
Raghd Alshoufani <121362316+Raghdkun@users.noreply.github.com>
2024-06-25 17:27:23 -0700

    add counter button

--- js/app.js
+++ js/app.js
@@ -2,9 +2,21 @@ const app = Vue.createApp({
     el: "hey",
     data() {
         return {
-            title : "The final empire"
+            title : "The final empire",
+            age : 0,
 
         }
     },
+    methods: {
+        increase : function inccrease() {
+            this.age++
+            
+        },
+        decrease : function inccrease() {
+            this.age--
+            
+        },
+    },
 });
+
 app.mount("#app");
